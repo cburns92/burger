bfee6eb5740fe399c7ea6b0752306795c532d7a9
cburns92 <cburns81592@gmail.com>
2018-11-04 20:01:49 -0500

    up and running

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require("../config/connection");
+
+var orm = {
+    selectAll: function(cb) {
+        var queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    insertOne: function(name, cb) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES ('" + name + "')";
+        console.log(queryString);
+        
+        connection.query(queryString, function(err,result){
+            if(err){
+                throw err;
+            }
+            cb(result)
+        });
+    },
+    updateOne: function(id, cb) {
+        var queryString = "UPDATE burgers SET ? WHERE ?";
+
+        connection.query(queryString, [{devoured:true}, {id:id}], function(err,result){
+            if(err){
+                throw err;
+            }
+            cb(result);
+        }) 
+    }
+}
+
+module.exports = orm;
